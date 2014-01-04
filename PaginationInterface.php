cefc552c52274c8926934ff990b877cb60d27439
Amy Stephen <AmyStephen@gmail.com>
2014-01-03 21:08:28 -0600

    .2

--- PaginationInterface.php
+++ PaginationInterface.php
@@ -3,7 +3,7 @@
  * Pagination Interface
  *
  * @package    Render
- * @copyright  2013 Common Api. All rights reserved.
+ * @copyright  2014 Amy Stephen. All rights reserved.
  * @license    http://www.opensource.org/licenses/mit-license.html MIT License
  */
 namespace CommonApi\Render;
@@ -13,8 +13,8 @@ namespace CommonApi\Render;
  *
  * @package    Render
  * @license    http://www.opensource.org/licenses/mit-license.html MIT License
- * @copyright  2013 Common Api. All rights reserved.
- * @since      0.1
+ * @copyright  2014 Amy Stephen. All rights reserved.
+ * @since      1.0
  */
 interface PaginationInterface
 {
--- RenderInterface.php
+++ RenderInterface.php
@@ -3,7 +3,7 @@
  * Render Interface
  *
  * @package    Render
- * @copyright  2013 Common Api. All rights reserved.
+ * @copyright  2014 Amy Stephen. All rights reserved.
  * @license    http://www.opensource.org/licenses/mit-license.html MIT License
  */
 namespace CommonApi\Render;
@@ -13,54 +13,17 @@ namespace CommonApi\Render;
  *
  * @package    Render
  * @license    http://www.opensource.org/licenses/mit-license.html MIT License
- * @copyright  2013 Common Api. All rights reserved.
- * @since      0.1
+ * @copyright  2014 Amy Stephen. All rights reserved.
+ * @since      1.0
  */
 interface RenderInterface
 {
     /**
-     * Render Theme and its include Page
+     * Render Output
      *
-     * @param   object $runtime_data
-     *
-     * @since   0.1
      * @return  string
+     * @since   1.0
+     * @throws  \CommonApi\Exception\RuntimeException
      */
-    public function includeTheme($runtime_data);
-
-    /**
-     * Parse rendered output looking for tokens (excluded tokens are head, held until all body rendered)
-     *
-     * @param   string $rendered_page
-     * @param   array  $exclude_tokens
-     *
-     * @return  array
-     * @since   0.1
-     */
-    public function parseTokens($rendered_page, array $exclude_tokens = array());
-
-    /**
-     * Render output for tag discovered in parsing
-     *
-     * @param   object      $runtime_data
-     * @param   object      $parameters
-     * @param   array       $query_results
-     * @param   null|object $model_registry
-     *
-     * @return  array
-     * @since   0.1
-     */
-    public function renderView($runtime_data, $parameters, array $query_results = array(), $model_registry = null);
-
-    /**
-     * Replace the token discovered during parsing with the associated rendered output
-     *
-     * @param   string $token
-     * @param   string $rendered_view
-     * @param   string $rendered_page
-     *
-     * @return  $this
-     * @since   0.1
-     */
-    public function injectRenderedOutput($token, $rendered_view, $rendered_page);
+    public function render();
 }
