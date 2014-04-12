0c9380a72d50bde6b9893b35a74c098a7c538271
AmyStephen <AmyStephen@gmail.com>
2014-04-12 10:43:55 -0500

    Pagination changes

--- PaginationInterface.php
+++ PaginationInterface.php
@@ -14,10 +14,35 @@ namespace CommonApi\Render;
  * @package    Render
  * @license    http://www.opensource.org/licenses/mit-license.html MIT License
  * @copyright  2014 Amy Stephen. All rights reserved.
- * @since      1.0
+ * @since      1.0.0
  */
 interface PaginationInterface
 {
+    /**
+     * Set pagination values
+     *
+     * << < 1 2 3 > >>
+     *
+     * @param  array  $data             Data to be displayed (not full results)
+     * @param  string $page_url         URL for page on which paginated appears
+     * @param  array  $query_parameters URL Query Parameters (other than page)
+     * @param  int    $total_items      Total items in full resultset for data
+     * @param  int    $per_page         Number of items per page
+     * @param  int    $display_links    Number of page number "buttons" to show
+     * @param  int    $page             Current page
+     *
+     * @since  1.0
+     */
+    public function setPagination(
+        array $data = array(),
+        $page_url,
+        array $query_parameters = array(),
+        $total_items,
+        $per_page,
+        $display_links,
+        $page
+    );
+
     /**
      * Get the first page number (always page=1)
      *
@@ -35,28 +60,12 @@ interface PaginationInterface
     public function getPrevPage();
 
     /**
-     * Get the first page number to use when looping through the display page number buttons
-     *
-     * @return  int
-     * @since   1.0
-     */
-    public function getStartDisplayPage();
-
-    /**
      * Get the current page number
      *
      * @return  int
      * @since   1.0
      */
     public function getCurrentPage();
-
-    /**
-     * Get the last page number to use when looping through the display page number buttons
-     *
-     * @return  int
-     * @since   1.0
-     */
-    public function getStopDisplayPage();
 
     /**
      * Get the page number following the current page
@@ -73,6 +82,22 @@ interface PaginationInterface
      * @since   1.0
      */
     public function getLastPage();
+
+    /**
+     * Get the first page number to use when looping through the display page number buttons
+     *
+     * @return  int
+     * @since   1.0
+     */
+    public function getStartLinksPage();
+
+    /**
+     * Get the last page number to use when looping through the display page number buttons
+     *
+     * @return  int
+     * @since   1.0
+     */
+    public function getStopLinksPage();
 
     /**
      * Get data paginated
@@ -93,13 +118,10 @@ interface PaginationInterface
     /**
      * Get the URL for the specified key
      *
-     *  - Use a numeric page number
-     *  - Or, use a specific key value: first, previous, current, next, or last
-     *
-     * @param   mixed $page
+     * @param   int $page_number
      *
      * @return  string
      * @since   1.0
      */
-    public function getPageUrl($page);
+    public function getPageUrl($page_number);
 }
