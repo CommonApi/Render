418898518d19b591ef31493695b9da0bf60dd36f
AmyStephen <AmyStephen@gmail.com>
2014-05-14 10:51:47 -0500

    Token Interface

---
+++ TokenInterface.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Token Interface
+ *
+ * @package    Token
+ * @copyright  2014 Amy Stephen. All rights reserved.
+ * @license    http://www.opensource.org/licenses/mit-license.html MIT License
+ */
+namespace CommonApi\Token;
+
+/**
+ * Token Interface
+ *
+ * @package    Token
+ * @license    http://www.opensource.org/licenses/mit-license.html MIT License
+ * @copyright  2014 Amy Stephen. All rights reserved.
+ * @since      1.0
+ */
+interface TokenInterface
+{
+    /**
+     * Token output for specified token and data
+     *
+     * @param   object $token
+     * @param   array  $data
+     *
+     * @return  string
+     * @since   1.0
+     */
+    public function processToken($token, array $data = array());
+}
