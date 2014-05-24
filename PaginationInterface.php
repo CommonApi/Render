187e5cf4f08f21ecf08495be60eddc6a3437dfe3
AmyStephen <AmyStephen@gmail.com>
2014-05-24 08:31:27 -0500

    Update PaginationInterface.php

--- PaginationInterface.php
+++ PaginationInterface.php
@@ -25,26 +25,26 @@ interface PaginationInterface
      *
      * << < 1 2 3 > >>
      *
-     * @param   int     $display_items_per_page_count
-     * @param   int     $display_page_link_count
-     * @param   boolean $create_sef_url_indicator
-     * @param   boolean $display_index_in_url_indicator
      * @param   int     $total_items
      * @param   string  $visited_page_url
      * @param   int     $current_start_parameter
      * @param   array   $other_query_parameters
+     * @param   int     $display_items_per_page_count
+     * @param   int     $display_page_link_count
+     * @param   boolean $create_sef_url_indicator
+     * @param   boolean $display_index_in_url_indicator
      *
      * @since   1.0
      * @return  object
      */
     public function getPaginationData(
-        $display_items_per_page_count = 5,
-        $display_page_link_count = 10,
-        $create_sef_url_indicator = false,
-        $display_index_in_url_indicator = true,
         $total_items,
         $visited_page_url,
         $current_start_parameter,
-        $other_query_parameters
+        $other_query_parameters,
+        $display_items_per_page_count = 5,
+        $display_page_link_count = 10,
+        $create_sef_url_indicator = false,
+        $display_index_in_url_indicator = true
     );
 }
